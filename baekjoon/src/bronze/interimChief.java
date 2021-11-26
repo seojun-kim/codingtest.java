35cf5afcc39f8b9bc8585a64a19770959fb1dee6
seojun <seojun1208@gmail.com>
2021-11-26 16:52:32 +0900

    실패

---
+++ baekjoon/src/bronze/interimChief.java
@@ -0,0 +1,21 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class interimChief {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        String[][] stuInformation = new String[Integer.parseInt(bf.readLine())][5];
+        String[] inputLine = new String[5];
+
+        for(int i = 0; i < stuInformation.length; i++) {
+
+        }
+
+    }
+}
